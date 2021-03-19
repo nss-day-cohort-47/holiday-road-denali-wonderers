171f444512562842b3a39476895508a926b4e316
David Darden <davidedarden83@gmail.com>
2021-03-19 10:56:39 -0500

    added footer and link

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -13,6 +13,7 @@ import { settings } from "../Settings.js"
 export const loadWeather = () => {
     return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=nashville&appid=${settings.weatherKey}`)
     .then(response => response.json())
+    //console.log(response.json())
     .then(parkWeather =>{
         console.log(parkWeather.list,"this is the park list")
         const parkCityWeather = parkWeather.list 
